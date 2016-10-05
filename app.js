5f39e980ea1c47304ccb2cd547749bde6ae60806
Adam Youngblood <ayoungbl0d@gmail.com>
2016-10-04 22:07:05 -0500

    Added $FULLTHING command and subsequent file

--- app.js
+++ app.js
@@ -19,6 +19,20 @@ discordjs.on('message', function(msg){
     });
   } else if (msg == "Hi Tai!") {
     msg.channel.sendMessage("Hello "+msg.author.username + ". Have you seen my 47 lambourghinis??");
+  } else if (msg == "$FULLTHING"){
+    msg.member.voiceChannel.join()
+    .then(function(connection){
+      connection.playFile('./fullthing.mp3')
+    setTimeout(function(){
+      msg.member.voiceChannel.leave();}, 200000);
+    });
+  } else if (msg == "$PYLONS"){
+    msg.member.voiceChannel.join()
+    .then(function(connection){
+      connection.playFile('./fullthing.mp3')
+    setTimeout(function(){
+      msg.member.voiceChannel.leave();}, 5000);
+    });
   }
 });
 
