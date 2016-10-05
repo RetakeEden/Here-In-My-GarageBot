a8a6aba31a77767aa5f75cf2bb408fcba832405a
Adam Youngblood <ayoungbl0d@gmail.com>
2016-10-04 22:01:27 -0500

    IT IS DONE

---
+++ app.js
@@ -0,0 +1,27 @@
+var Discord = require('discord.js');
+var config = require('./config.json')
+
+var discordjs = new Discord.Client();
+
+
+discordjs.on("ready", function() {
+  console.log('Currently on. Connected to ' + discordjs.channels);
+});
+
+
+discordjs.on('message', function(msg){
+  if (msg == "$KNAWLEDGE") {
+    msg.member.voiceChannel.join()
+    .then(function(connection){
+      connection.playFile('./himg.mp3')
+    setTimeout(function(){
+      msg.member.voiceChannel.leave();}, 5000);
+    });
+  } else if (msg == "Hi Tai!") {
+    msg.channel.sendMessage("Hello "+msg.author.username + ". Have you seen my 47 lambourghinis??");
+  }
+});
+
+
+
+discordjs.login(config.info.email, config.info.password);
