3776600c5620ffd8e607989a4746462ef9f4caa1
Adam Youngblood <ayoungbl0d@gmail.com>
2016-10-06 10:15:54 -0500

    Heroku stuff

--- app.js
+++ app.js
@@ -5,48 +5,57 @@ var discordjs = new Discord.Client();
 
 
 discordjs.on("ready", function() {
-  console.log('Currently on. Connected to ' + discordjs.channels);
+  console.log('Currently on. Connected.');
 });
 
 
 discordjs.on('message', function(msg){
-  if (msg == "$knowledge") {
-    msg.member.voiceChannel.join()
-    .then(function(connection){
-      connection.playFile('./himg.mp3')
-    setTimeout(function(){
-      msg.member.voiceChannel.leave();}, 5000);
-    });
-  } else if (msg == "Hi Tai!") {
-    msg.channel.sendMessage("Hello "+msg.author.username + ". Have you seen my 47 lambourghinis??");
-  } else if (msg == "$FULLTHING"){
-    msg.member.voiceChannel.join()
-    .then(function(connection){
-      connection.playFile('./fullthing.mp3')
-    setTimeout(function(){
-      msg.member.voiceChannel.leave();}, 200000);
-    });
-  } else if (msg == "$PYLONS"){
-    msg.member.voiceChannel.join()
-    .then(function(connection){
-      connection.playFile('./fullthing.mp3')
-    setTimeout(function(){
-      msg.member.voiceChannel.leave();}, 5000);
-    });
-  } else if (msg == "$KNAWLEDGE") {
-    msg.member.voiceChannel.join()
-    .then(function(connection){
-      connection.playFile('./garaaaage.mp3')
-    setTimeout(function(){
-      msg.member.voiceChannel.leave();}, 3000);
-    });
-  } else if (msg =="$DREARS") {
-    msg.member.voiceChannel.join()
-    .then(function(connection){
-      connection.playFile('./drears.mp3')
-    setTimeout(function(){
-      msg.member.voiceChannel.leave();}, 3000);
-    });
+  if(msg.member.voiceChannel){
+    if (msg == "$knowledge") {
+      console.log("knowledge was called")
+      msg.member.voiceChannel.join()
+      .then(function(connection){
+        connection.playFile('./himg.mp3')
+      setTimeout(function(){
+        msg.member.voiceChannel.leave();}, 5000);
+      });
+    } else if (msg == "Hi Tai!") {
+      msg.channel.sendMessage("Hello "+msg.author.username + ". Have you seen my 47 lambourghinis??");
+      console.log("Hi was called");
+    } else if (msg == "$FULLTHING"){
+      console.log("fullthing was called");
+      msg.member.voiceChannel.join()
+      .then(function(connection){
+        connection.playFile('./fullthing.mp3')
+      setTimeout(function(){
+        msg.member.voiceChannel.leave();}, 200000);
+      });
+    } else if (msg == "$PYLONS"){
+      msg.member.voiceChannel.join()
+      .then(function(connection){
+        connection.playFile('./fullthing.mp3')
+      setTimeout(function(){
+        msg.member.voiceChannel.leave();}, 5000);
+      });
+    } else if (msg == "$KNAWLEDGE") {
+      console.log("knawledge was called");
+      msg.member.voiceChannel.join()
+      .then(function(connection){
+        connection.playFile('./garaaaage.mp3')
+      setTimeout(function(){
+        msg.member.voiceChannel.leave();}, 3000);
+      });
+    } else if (msg =="$DREARS") {
+      msg.member.voiceChannel.join()
+      .then(function(connection){
+        connection.playFile('./drears.mp3')
+      setTimeout(function(){
+        msg.member.voiceChannel.leave();}, 3000);
+      });
+    }
+  } else {
+    msg.channel.sendMessage("I can't tell you about my self-help if you don't join a voice channel, "+ msg.author.username + "!")
+    console.log("user had no voice channel")
   }
 });
 
