f98907255c5b1ee14123dfb226538311e561cdb8
Adam Youngblood <ayoungbl0d@gmail.com>
2016-10-10 21:49:40 -0500

    Testing a few changes

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var config = require('./config.json')
 
 var discordjs = new Discord.Client();
 
+var recent = true;
 
 discordjs.on("ready", function() {
   console.log('Currently on. Connected.');
@@ -10,23 +11,27 @@ discordjs.on("ready", function() {
 
 
 discordjs.on('message', function(msg){
+  msg = msg.toUpperCase();
   if(msg.member.voiceChannel){
-    if (msg == "$knowledge") {
+    if (msg == "$KNOWLEDGE") {
       console.log("knowledge was called")
       msg.member.voiceChannel.join()
       .then(function(connection){
         connection.playFile('./himg.mp3')
+        recent = true;
       setTimeout(function(){
         msg.member.voiceChannel.leave();}, 5000);
       });
-    } else if (msg == "Hi Tai!") {
+    } else if (msg == "HI TAI!") {
       msg.channel.sendMessage("Hello "+msg.author.username + ". Have you seen my 47 lambourghinis??");
+      recent = false;
       console.log("Hi was called");
     } else if (msg == "$FULLTHING"){
       console.log("fullthing was called");
       msg.member.voiceChannel.join()
       .then(function(connection){
         connection.playFile('./fullthing.mp3')
+        recent = false;
       setTimeout(function(){
         msg.member.voiceChannel.leave();}, 200000);
       });
@@ -35,21 +40,20 @@ discordjs.on('message', function(msg){
       msg.member.voiceChannel.join()
       .then(function(connection){
         connection.playFile('./garaaaage.mp3')
+        recent = false;
       setTimeout(function(){
         msg.member.voiceChannel.leave();}, 3000);
       });
-    } else if (msg =="$DREARS") {
-      msg.member.voiceChannel.join()
-      .then(function(connection){
-        connection.playFile('./drears.mp3')
-      setTimeout(function(){
-        msg.member.voiceChannel.leave();}, 3000);
-      });
+    } else if (msg == "TAI PLS GO"){
+      msg.member.voiceChannel.leave();
     }
   } else {
-    msg.channel.sendMessage("I can't tell you about my self-help if you don't join a voice channel, "+ msg.author.username + "!")
-    console.log("user had no voice channel");
-    return "no voice channel";
+    if (recent){
+      msg.channel.sendMessage("I can't tell you about my self-help if you don't join a voice channel, "+ msg.author.username + "!")
+      console.log("user had no voice channel");
+      return "no voice channel";
+      recent = true;
+    }
   }
 });
 
