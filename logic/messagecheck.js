0db945392cf94c0bdf98e64358cb49991dd16678
Adam Youngblood <ayoungbl0d@gmail.com>
2016-10-27 20:47:25 -0500

    too much ruby lately

--- logic/messagecheck.js
+++ logic/messagecheck.js
@@ -129,10 +129,10 @@ function noGo(msg){
 
 //Returns the method key in a string that matches
 //the command passed in by toCall
-function returnMethod(msg){
+function returnMethod(x){
   //checks in keys if a message matches a key value
   for (i in keys) {
-    if msg == keys[i]{
+    if (x == keys[i]){
       return key[i].toString();
     }
   }
