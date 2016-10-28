d63347f8489b52230890ade8a42b67b209e7792e
Adam Youngblood <ayoungbl0d@gmail.com>
2016-10-27 20:46:09 -0500

    bugfix 2, bugfix harder

--- logic/messagecheck.js
+++ logic/messagecheck.js
@@ -129,10 +129,10 @@ function noGo(msg){
 
 //Returns the method key in a string that matches
 //the command passed in by toCall
-function returnMethod(x){
+function returnMethod(msg){
   //checks in keys if a message matches a key value
   for (i in keys) {
-    if x == keys[i]{
+    if msg == keys[i]{
       return key[i].toString();
     }
   }
