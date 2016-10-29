3c12c058f1fed8febc14dc377d71d9a5735a47bd
Adam Youngblood <ayoungbl0d@gmail.com>
2016-10-29 09:54:40 -0500

    fix test

--- logic/messagecheck.js
+++ logic/messagecheck.js
@@ -131,7 +131,7 @@ function noGo(msg){
 //the command passed in by toCall
 function returnMethod(x){
   x = x.toLowerCase()
-  console.log(x)
+  x = x.split('').slice(0, 1).join('')
   //checks in keys if a message matches a key value
   for (var i = 0; i <= keys.length; i++) {
     console.log(keys);
