68022536af17f6191208786d97c0e32ee40e59e6
Adam Youngblood <ayoungbl0d@gmail.com>
2016-12-07 13:40:09 -0600

    basis for queuing music

--- logic/ytlogic.js
+++ logic/ytlogic.js
@@ -1,6 +1,8 @@
 var config = require('../config.json'),
     ytdl = require('ytdl-core'),
-    request = require('request');
+    request = require('request'),
+    search = [],
+    searchname = [];
 
 function base(passed, msg){
   if (passed[0] == "\'"){
@@ -9,27 +11,44 @@ function base(passed, msg){
     passed = passed.toLowerCase();
     var final = passed.replace(/\"/g, "").split(' ').join(',');
   }
-  request(`https://www.googleapis.com/youtube/v3/search?part=snippet&q=${final}&key=${config.info.apiKEY}`, function(err, res, body){
+  request(`https://www.googleapis.com/youtube/v3/search?part=snippet&q=${final}&maxResults=1&key=${config.info.apiKEY}`, function(err, res, body){
     var testbody = JSON.parse(body)
-    var search = testbody.items[0].id.videoId;
-    ytpb(search, msg);
+    console.log(testbody);
+    search.push(testbody.items[0].id.videoId);
+    console.log(testbody.items[1]);
+    console.log(testbody.items[1].title)
+    searchname.push(testbody.items[1].title);
+    msg.channel.sendMessage("")
+    ytpb(msg);
   })
 }
 
-function ytpb(search, msg){
-  var streamOptions = { seek: 0, volume: 1 };
+function queue(msg){
+  msg.channel.sendMessage("The current queue is: " + searchname)
+}
+
+function ytpb(msg){
   if (msg.member.voiceChannel){
     msg.member.voiceChannel.join()
     .then(function(connection){
-      var stream = ytdl(`https://www.youtube.com/watch?v=${search}`, {filter: "audioonly"})
-      connection.playStream(stream, streamOptions);
+      queued(connection, msg);
     })
-    .catch(console.error)
   } else {
-    msg.channel.sendMessage(`https://www.youtube.com/watch?v=${search}`)
+    msg.channel.sendMessage("You're not in a voice channel!")
   }
 }
 
+function queued(conn, msg){
+  var streamOptions = { seek: 0, volume: 1 };
+  var stream = ytdl(`https://www.youtube.com/watch?v=${search[0]}`, {filter: "audioonly"})
+  msg.channel.sendMessage("Now playing: " + searchname[0]);
+  var disp = conn.playStream(stream, streamOptions);
+  disp.on('end', function(){
+    search.splice(0,1);
+    searchname.splice(0,1);
+    ytpb(msg);
+  })
+}
 
 module.exports = {
   base: base,
