4586da85dd046e7de2bcb654db391cb8a6f6334a
Adam Youngblood <ayoungbl0d@gmail.com>
2016-12-06 19:25:46 -0600

    basic yt checks

--- logic/ytlogic.js
+++ logic/ytlogic.js
@@ -1,8 +1,22 @@
-var ytdl = require('ytdl-core');
-var request = require('request');
+var config = require('../config.json'),
+    ytdl = require('ytdl-core'),
+    request = require('request');
 
-function base(){
-
+function base(passed){
+  var final;
+  console.log(passed, "passed one");
+  if (passed[0] == "\'"){
+    return;
+  } else {
+    passed = passed.toLowerCase();
+    final = passed.replace(/\"/g, "").split(' ').join(',');
+  }
+  console.log(final, "final one")
+  request(`https://www.googleapis.com/youtube/v3/search?part=snippet&q=${final}&key=${config.info.apiKEY}`, function(err, res, body){
+    console.log(res)
+    console.log("=========================")
+    console.log(JSON.stringify(res));
+  })
 }
 
 
