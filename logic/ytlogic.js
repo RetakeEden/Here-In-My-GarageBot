083342806f7c2a6baf871072d4870dc3a5e96abc
Adam Youngblood <ayoungbl0d@gmail.com>
2016-12-21 22:58:49 -0600

    I think I got it

--- logic/ytlogic.js
+++ logic/ytlogic.js
@@ -15,16 +15,18 @@ function base(passed, msg, clie){
     var final = passed.replace(/\"/g, "").split(' ').join(',');
   }
   request(`https://www.googleapis.com/youtube/v3/search?part=snippet&q=${final}&maxResults=1&key=${config.info.apiKEY}`, function(err, res, body){
-    var testbody = JSON.parse(body)
-    // console.log(testbody);
-    // console.log(testbody.items[0].snippet);
-    // console.log(testbody.items[0].id);
-    // var dur = parse.convertTime("PT4M13S");
-    // console.log(dur*1000);
-    search.push(testbody.items[0].id.videoId);
-    searchname.push(testbody.items[0].snippet.title);
-    msg.channel.sendMessage("\"" +testbody.items[0].snippet.title + "\" has been added to queue.")
-    ytpb(msg);
+    if (search.length == 0) {
+      var testbody = JSON.parse(body)
+      search.push(testbody.items[0].id.videoId);
+      searchname.push(testbody.items[0].snippet.title);
+      msg.channel.sendMessage("\"" +testbody.items[0].snippet.title + "\" has been added to queue.")
+      ytpb(msg);
+    } else {
+      var testbody = JSON.parse(body)
+      search.push(testbody.items[0].id.videoId);
+      searchname.push(testbody.items[0].snippet.title);
+      msg.channel.sendMessage("\"" +testbody.items[0].snippet.title + "\" has been added to queue.")
+    }
   })
 }
 
