c0fd1936b74544246d1e16d303c4d009a556fa8d
Adam Youngblood <ayoungbl0d@gmail.com>
2017-01-09 14:32:20 -0600

    another minor typo

--- logic/palogic.js
+++ logic/palogic.js
@@ -10,7 +10,7 @@ var pandora = new pan(`${config.info.panlog}`, `${config.info.panpass}`);
 function plogin(msg){
   pandora.login(function(err){
     if(err) {console.log(err)};
-    pandora.requrest(function(err, stations){
+    pandora.request(function(err, stations){
       if(err) {console.log(err)};
       msg.channel.sendMessage("Current Pandora Stations: " + stations);
     })
