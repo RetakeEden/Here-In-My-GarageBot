813cb7ec9a3f2e5b4fc6b9310f2c6fd0740a6f4d
Adam Youngblood <ayoungbl0d@gmail.com>
2017-01-09 15:30:51 -0600

    more bug fix attempts

--- logic/rlogic.js
+++ logic/rlogic.js
@@ -2,21 +2,23 @@ var config = require('../config.json'),
     request = require('request'),
     parse = require('./parse.js');
 
+var data = {
+  "jsonrpc": "2.0",
+  "method": "generateIntegers",
+  "params": {
+      "apiKey": `${config.info.rapi}`,
+      "n": 1,
+      "min": 1,
+      "max": 100,
+      "replacement": true
+  },
+  "id": 1
+}
+
 var random = {
   "url": "https://api.random.org/json-rpc/1/invoke",
   "method": "POST",
-  form: {
-    "jsonrpc": "2.0",
-    "method": "generateIntegers",
-    "params": {
-        "apiKey": `${config.info.rapi}`,
-        "n": 1,
-        "min": 1,
-        "max": 100,
-        "replacement": true
-    },
-    "id": 1
-  }
+  form: JSON.stringify(data);
 }
 
 function rng(msg){
