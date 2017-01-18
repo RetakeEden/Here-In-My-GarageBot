76527d02b6fd058502c34d84b01cb4051d951177
Adam Youngblood <ayoungbl0d@gmail.com>
2017-01-17 18:38:13 -0600

    Trying this insteaD

--- logic/palogic.js
+++ logic/palogic.js
@@ -3,20 +3,22 @@ var config = require('../config.json'),
     parse = require('./parse.js'),
     curconn = '',
     disp = null,
-    base = "http://tuner.pandora.com/services/json/";
+    base = "https://tuner.pandora.com/services/json/";
 
 
 var partinfo = {
   "username": "android",
   "password": "AC7IBG09A3DTSYM4R41UJWL07VLN8JI7",
   "deviceModel": "android-generic",
-  "version": "5"
 }
 
 var loginfo = {
-  "uri": base + "?method=auth.partnerLogin",
   "method": "POST",
-  form: JSON.stringify(partinfo)
+  "url": base,
+  "qs": {
+    "method": "auth.partnerLogin"
+  },
+  "body": JSON.stringify(partinfo)
 }
 function plogin(msg){
   request(loginfo, function(err, res, body){
