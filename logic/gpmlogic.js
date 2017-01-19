978120dd9b111679cd81f9b423a43918f63ff825
Adam Youngblood <ayoungbl0d@gmail.com>
2017-01-19 01:11:50 -0600

    bug with promises

--- logic/gpmlogic.js
+++ logic/gpmlogic.js
@@ -93,20 +93,20 @@ function playCurr(msg) {
 
 function clearq(msg){
   new Promise(function(resolve, reject){
-    disp.end();
     curconn = null;
-    console.log('hit 1 98');
+    console.log('hit 1 97');
     resolve();
   })
   .then(function(){
     new Promise(function(resolve, reject){
       disp = null;
       jclear = true;
-      console.log('hit 2 105');
+      console.log('hit 2 104');
+      resolve();
     })
-    resolve();
   })
   .then(function(){
+    disp.end();
     console.log('hit 3 110');
     playCurr(msg);
   })
