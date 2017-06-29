5659d81f7bf2fc311d5813192f42cef476560ac6
Adam Youngblood <ayoungbl0d@gmail.com>
2017-06-28 23:33:22 -0500

    minor changes

--- app.js
+++ app.js
@@ -17,9 +17,9 @@ discordjs.on("ready", () => {
 
 //called on every message
 discordjs.on('message', msg => {
-  if (msg.author.username == "nik4375#9300"){
-    msg.react(":poop:");
-    return;
+  if (msg.author.username == "nik4375"){
+    msg.react(💩);
+    return "reacted to nicki";
   }
   if (msg.author.username == "Tai Lopez"){
     return;
